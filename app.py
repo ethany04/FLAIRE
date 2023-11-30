465b03aa2bc020ef5e7c190434c6c8f1fb234c92
tusharc110 <tushar12chaudhary@utexas.edu>
2023-11-29 20:15:13 -0600

    finished model and basic interface

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import io
 from model import CNNModel
 from werkzeug.utils import secure_filename
 import time
+import os
 
 app = Flask(__name__)
+
 
 label_mapping = {0: 'Accident', 1: 'Non Accident'}
 
@@ -28,12 +30,15 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'frame' not in request.files:
+    if 'frames' not in request.files:
         return 'No file part'
-    file = request.files['frame']
-    if file.filename == '':
-        return 'No selected file'
-    if file:
+    
+    files = request.files.getlist('frames')
+
+    results = []
+    for file in files:
+        if file.filename == '':
+            return 'No selected file'
         filename = secure_filename(file.filename)
         image_bytes = file.read()
         image = Image.open(io.BytesIO(image_bytes))
@@ -52,8 +57,10 @@ def upload_file():
         total_time = end_time - start_time
         fps = 1.0 / total_time if total_time > 0 else "NaN"
 
+        results.append((filename, prediction_label, total_time, fps))
 
-    return render_template('result.html', label=prediction_label, time=total_time, fps=fps)
+
+    return render_template('result.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
